2adbb6b3d688eb2e8bbf53825647365294b287c9
njwidmann <njwidmann@gmail.com>
2019-01-08 16:35:09 -0500

    Update accelerometer.ino

--- accelerometer.ino
+++ accelerometer.ino
@@ -1,7 +1,6 @@
 #include <KalmanSmooth.h>
 
 const int zInput = A5;
-const int reference = A1;
  
 // Take multiple samples to reduce noise
 const int initialCalibrationSampleSize = 100;
@@ -10,14 +9,11 @@ float z0 = 0;
 float z_raw = 0;
 float z_smoothed = 0;
 float z_smoothed2 = 0;
-float ref0 = 0;
 float Q_z = 0.001;
 float R_z = 0.1;
 
 KalmanSmooth kalman_filter_z(Q_z,R_z,1);
 KalmanSmooth kalman_filter_z2(Q_z,R_z,1);
-KalmanSmooth kalman_filter_ref(Q_z,R_z,1);
-KalmanSmooth kalman_filter_ref2(Q_z,R_z,1);
 
 void setupAccelerometer() 
 {
@@ -25,10 +21,8 @@ void setupAccelerometer()
   //calibrate z0
   for(int i = 0; i < initialCalibrationSampleSize; i++) {
     z0+=ReadAxis(zInput);
-    ref0+=ReadAxis(reference);
   }
   z0 = z0 / (initialCalibrationSampleSize);
-  ref0 = ref0 / (initialCalibrationSampleSize);
   
 }
 
@@ -37,12 +31,10 @@ void updateAccelerometer()
   z_raw = ReadAxis(zInput);
   
   z_raw = z_raw - z0;
-
+  //filter once
   z_smoothed = kalman_filter_z.smooth(z_raw);
-  
+  //filter twice
   z_smoothed2 = kalman_filter_z2.smooth(z_smoothed);
-
-  //z_smoothed2 -= kalman_filter_ref2.smooth(kalman_filter_ref.smooth(ReadAxis(reference) - ref0));
  
 }
 
@@ -57,10 +49,8 @@ int ReadAxis(int axisPin)
 {
   long reading = 0;
   analogRead(axisPin);
-  //analogRead(reference);
   for (int i = 0; i < sampleSize; i++)
   {
-    //reading += (analogRead(axisPin) - analogRead(reference));
     reading += (analogRead(axisPin));
   }
   return reading/sampleSize - 512;
